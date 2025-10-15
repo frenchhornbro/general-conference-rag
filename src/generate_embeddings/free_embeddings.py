82ac2afa90488e67e963a09265e62b0f2140e1c3
Hyrum Durfee <htdurfee@gmail.com>
2025-10-15 10:31:44 -0600

    update(free_embeddings) added embedding of just titles

--- src/generate_embeddings/free_embeddings.py
+++ src/generate_embeddings/free_embeddings.py
@@ -10,7 +10,7 @@ with open("config.json") as config:
   raw_dir = c["raw_dir"]
   embeddings_dir = c["embeddings_dir"]
 
-def generate_embeddings(csv_file, column_name, output_dir):
+def generate_embeddings(csv_file, output_file_name, output_dir, col_to_embed = "text"):
   os.makedirs(output_dir, exist_ok=True)
   try:
     df = pd.read_csv(csv_file)
@@ -23,7 +23,7 @@ def generate_embeddings(csv_file, column_name, output_dir):
     else:
       print("Using CPU for encoding")
 
-    texts = df['text'].tolist()
+    texts = df[col_to_embed].tolist()
     embeddings = model.encode(
       texts,
       batch_size=32,
@@ -33,9 +33,9 @@ def generate_embeddings(csv_file, column_name, output_dir):
     ).tolist()
 
     df['embedding'] = embeddings
-    output_file = os.path.join(output_dir, f"{column_name}.csv")
-    df.to_csv(output_file, index=False)
-    print(f"Embeddings generated and saved to '{output_file}'")
+    output_file_name = os.path.join(output_dir, f"{output_file_name}.csv")
+    df.to_csv(output_file_name, index=False)
+    print(f"Embeddings generated and saved to '{output_file_name}'")
   except Exception as e:
     print(f"An error occurred: \033[35m{e}\033[0m")
 
@@ -45,4 +45,6 @@ if __name__ == "__main__":
   generate_embeddings(f"{raw_dir}/SCRAPED_TALKS.csv", "talk", f"{embeddings_dir}/free")
   print("Start paragraphs: ", datetime.now().strftime("%H:%M:%S"))
   generate_embeddings(f"{raw_dir}/SCRAPED_PARAGRAPHS.csv", "paragraph", f"{embeddings_dir}/free")
+  print("Start titles: ", datetime.now().strftime("%H:%M:%S"))
+  generate_embeddings(f"{raw_dir}/SCRAPED_TALKS.csv", "title", f"{embeddings_dir}/free", "title")
   print("Finish: ", datetime.now().strftime("%H:%M:%S"))
